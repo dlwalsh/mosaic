3077e28878acebeb742e9bce14d1b5c59c490c6a
David Walsh <dlwalsh@gmail.com>
2016-02-06 16:06:19 +1100

    working with callbacks

--- js/client.js
+++ js/client.js
@@ -1,36 +1,34 @@
 var TILE_WIDTH = TILE_WIDTH || 16;
 var TILE_HEIGHT = TILE_HEIGHT || 16;
 
-function readFile(file, callback) {
+function readImage(file, callback) {
 
     var reader = new FileReader();
 
-    reader.addEventListener('load', function (event) {
+    reader.addEventListener('load', function (readerEvent) {
 
         var image = new Image();
-        var ctx = document.createElement('canvas').getContext('2d');
-        image.src = event.target.result;
+        image.src = readerEvent.target.result;
 
-        image.addEventListener('load', function () {
+        image.addEventListener('load', function (imageEvent) {
 
-            var bytes, length, i, rgbSum, avgColor;
+            var ctx, h, w, img, row, matrix;
 
-            ctx.drawImage(image, 0, 0);
-            var data = ctx.getImageData(0, 0, TILE_WIDTH, TILE_HEIGHT).data;
-            var rgbSum = [0, 0, 0];
+            img = imageEvent.target;
+            ctx = document.createElement('canvas').getContext('2d');
+            ctx.drawImage(img, 0, 0);
 
-            for (i = 0; i < data.length; i += 4) {
-                rgbSum[0] += data[i];
-                rgbSum[1] += data[i + 1];
-                rgbSum[2] += data[i + 2];
+            matrix = [];
+
+            for (w = 0; w < img.width; w += TILE_WIDTH) {
+                row = [];
+                for (h = 0; h < img.height; h += TILE_HEIGHT) {
+                    row.push(averageColor(ctx.getImageData(w, h, TILE_WIDTH, TILE_HEIGHT).data));
+                }
+                matrix.push(row);
             }
 
-            var avgColor = rgbSum.map(function (sum) {
-                var average = Math.round(sum * 4 / data.length);
-                return average.toString(16);
-            }).join('');
-
-            console.log(avgColor);
+            callback(matrix);
 
         });
 
@@ -40,15 +38,41 @@ function readFile(file, callback) {
 
 }
 
+function averageColor(bytes) {
+
+    var rgbSum, i,
+        INCREMENT = 4;
+
+    if (bytes.length % INCREMENT !== 0) { // Must be a sequence of quadruples (R, G, B, A)
+        throw Error('Pixel bytes not a multiple of four');
+    }
+
+    rgbSum = [0, 0, 0];
+
+    for (i = 0; i < bytes.length; i += INCREMENT) {
+        rgbSum[0] += bytes[i];
+        rgbSum[1] += bytes[i + 1];
+        rgbSum[2] += bytes[i + 2];
+    }
+
+    return rgbSum.map(function (sum) {
+        var average = Math.round(sum * INCREMENT / bytes.length);
+        var hex = average.toString(16);
+        return average < 0x0F ? '0' + hex : hex;
+    }).join('');
+
+}
+
+function createDisplayer(elem) {
+    return function (msg) {
+        elem.innerHTML = msg;
+    };
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 
     var input = document.querySelector('#imageInput');
-
-    var displayError = (function (elem) {
-        return function (msg) {
-            elem.innerHTML = msg;
-        };
-    }(document.querySelector('#errorMessage')));
+    var displayError = createDisplayer(document.querySelector('#errorMessage'));
 
     input.addEventListener('change', function (event) {
 
@@ -65,7 +89,9 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         displayError('');
-        readFile(files[0]);
+        readImage(files[0], function (colorMatrix) {
+            console.log(colorMatrix);
+        });
 
     });
 
